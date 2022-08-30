f0c9e085207a969a0617564d770562cf7377ebff
Lim Ji Siang <limjisiang99@gmail.com>
2022-08-30 09:44:12 +0100

    Did some house keeping

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -3,6 +3,8 @@ using Domain;
 using Persistence;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
 namespace Application.Activities{
 
     public class List
@@ -12,11 +14,14 @@ namespace Application.Activities{
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
             private readonly DataContext _context;
+           
             public Handler(DataContext context){
                 _context = context;
+            
             }
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationtoken){
-                return await _context.Activities.ToListAsync();
+
+                return await _context.Activities.ToListAsync(cancellationtoken);
             }
         }
     }
